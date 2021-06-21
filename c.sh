3f908a71c6a2b1cbcc2643cb82d0f92c864e0e74
pingme998 <77038591+pingme998@users.noreply.github.com>
2021-06-21 15:36:57 +0530

    Update c.sh

--- c.sh
+++ c.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm co.sh
-curl 'https://raw.githubusercontent.com/pingme998/gistxtforcow/main/cowin.sh' >co.sh
+#curl 'https://raw.githubusercontent.com/pingme998/gistxtforcow/main/cowin.sh' >co.sh
 while :
 do
 	bash co.sh
