2f6f5272fc3e0303985bbb7c4ba3678ac6f30b82
pingme998 <77038591+pingme998@users.noreply.github.com>
2021-06-21 15:33:23 +0530

    Update cowin.sh

--- cowin.sh
+++ cowin.sh
@@ -2,7 +2,7 @@
 curl 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=695304&date=16-06-2021' >f.txt
 grep -o '695304\|695301' f.txt >f2.txt
 sed "s|^|https://api.telegram.org/bot1669913040:AAHZ6Wl5SiWiUuyA8QvlJ55oadXZeYmRGQ0/sendMessage?chat_id=-587879925\&text=|" f2.txt >f3.txt
-sleep 5
+sleep 2
 curl … $(cat f3.txt)
 rm *.txt
-sleep 5
+sleep 2
