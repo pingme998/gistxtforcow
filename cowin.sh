76a9932b2547eb558e3dc1d1eb3a9323c31ab64c
pingme998 <77038591+pingme998@users.noreply.github.com>
2021-06-21 15:38:02 +0530

    Update cowin.sh

--- cowin.sh
+++ cowin.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+rm co.sh
+curl 'https://raw.githubusercontent.com/pingme998/gistxtforcow/main/cowin.sh' >co.sh
+
 curl 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=695304&date=16-06-2021' >f.txt
 grep -o '695304\|695301' f.txt >f2.txt
 sed "s|^|https://api.telegram.org/bot1669913040:AAHZ6Wl5SiWiUuyA8QvlJ55oadXZeYmRGQ0/sendMessage?chat_id=-587879925\&text=|" f2.txt >f3.txt
