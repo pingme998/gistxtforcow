e0f2821fb259184241672c7f70d274f0470c22c7
pingme998 <77038591+pingme998@users.noreply.github.com>
2021-06-25 14:58:49 +0530

    Update cowin.sh

--- cowin.sh
+++ cowin.sh
@@ -2,7 +2,7 @@
 #hello13
 while :
 do
-      curl 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=695304&date=25-06-2021' >f.txt
+      curl 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=695304&date=26-06-2021' >f.txt
       grep -o '695304' f.txt >f2.txt
       sed "s|^|https://api.telegram.org/bot1669913040:AAHZ6Wl5SiWiUuyA8QvlJ55oadXZeYmRGQ0/sendMessage?chat_id=-587879925\&text=|" f2.txt >f3.txt
       sleep 5
